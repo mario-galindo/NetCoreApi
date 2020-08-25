4795637268ecf4b00b151a0ca42dd321e2cd786f
mario-galindo <mgalindo9410@gmail.com>
2020-08-25 11:15:33 -0600

    add properties to entity class

--- NetCoreApi/Product.cs
+++ NetCoreApi/Product.cs
@@ -7,5 +7,9 @@ namespace NetCoreApi
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
     }
 }
