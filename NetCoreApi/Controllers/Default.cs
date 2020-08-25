3a137a66084dde09aceb646398c1e154be72bdfa
mario-galindo <mgalindo9410@gmail.com>
2020-08-25 11:47:33 -0600

    change default route on startup add method for get all and get single products

---
+++ NetCoreApi/Controllers/Default.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class Default : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Running...";
+        }
+
+    }
+}
--- NetCoreApi/Controllers/ProductController.cs
+++ NetCoreApi/Controllers/ProductController.cs
@@ -10,6 +10,18 @@ namespace NetCoreApi.Controllers
     [Route("products")]
     public class ProductController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<List<Product>> GetAll()
+        {
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            return Products.Single(x => x.Id == id);
+        }
+
         public static List<Product> Products = new List<Product>
         {
             new Product
